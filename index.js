aef517f410cb80d1281aac56cd7fb28c2ae434f9
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-07-14 08:24:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,40 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+
 const app = express();
-
 app.use(cors());
+app.use(express.json());
 
-app.use(express.json()); // to parse JSON body
-
-const processRequest = async (req, res, next, url, headers) => {
+app.all('/fetchdata', async (req, res) => {
   try {
-    if (!url) {
-      throw new Error("URL is required");
-    }
-
+    const { url, method, headers, body } = req.body;
+    
+    // Make the request to the target URL
     const response = await axios({
-      url: url,
-      method: req.method,
-      headers: headers,
-      data: req.method === 'POST' ? req.body : {},
+      url,
+      method: method || 'GET',
+      headers: headers || {},
+      data: body || {},
     });
-
-    if (response.status >= 200 && response.status < 300) {
-      res.send(response.data);
-    } else {
-      throw new Error(`Request to ${url} returned status ${response.status}`);
-    }
+    
+    // Forward the response back to the client
+    res.send(response.data);
   } catch (error) {
-    next(error);
+    console.error('Error occurred:', error.message);
+    
+    // Handle any errors and send an error response
+    const statusCode = error.response?.status || 500;
+    const errorMessage = error.message || 'Internal Server Error';
+    res.status(statusCode).send({
+      message: errorMessage,
+      status: 'error',
+      statusCode,
+    });
   }
-};
-
-// Handler for GET requests
-app.get('/fetchdata', async (req, res, next) => {
-  const { url } = req.query;
-  await processRequest(req, res, next, url);
-});
-
-// Handler for POST requests
-app.post('/fetchdata', async (req, res, next) => {
-  const { url, headers } = req.body;
-  await processRequest(req, res, next, url, headers);
-});
-
-// Error handling middleware
-app.use((error, req, res, next) => {
-  console.error(`Error occurred: ${error.message}`);
-
-  const statusCode = error.response?.status || 500;
-  const errorMessage = error.message || 'Internal Server Error';
-
-  res.status(statusCode).send({
-    message: errorMessage,
-    status: 'error',
-    statusCode,
-  });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`CORS proxy server running on http://localhost:${port}`);
 });
